df4e26a689916b39ef574d3dd7efceb6b544a3a9
Daniel <danil.malaev3@gmail.com>
2026-06-28 19:48:16 +0300

    add: handlers template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from models import  Product, Category
 
-# Create your views here.
+class ProductListView(): ...
+
+class ProductDetailBiew(): ...
