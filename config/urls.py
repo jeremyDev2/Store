9772a18fac5a1be4a29ce148cef52c24016c23d1
Daniel <danil.malaev3@gmail.com>
2026-06-30 19:37:30 +0300

    add: reviews & orders urls

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("products/", include("products.urls")),
+    path('reviews/', include('reviews.urls')),
+    path('orders/', include("orders.urls")),
 ]
