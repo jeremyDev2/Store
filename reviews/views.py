e6e4454f49ffe7809b791cbc9521765ae29683fd
Daniel <danil.malaev3@gmail.com>
2026-06-30 19:21:14 +0300

    add: 'post' handler for adding review

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views import View
+from products.models import Product
+from orders.models import OrderItem
+from .models import Review
 
-# Create your views here.
+class AddReviewView(View):
+
+    def post(self, request, product_id):
+        
+        product = get_object_or_404(Product, id=product_id)
+        #check if user buy something. exist() - check DB and if we have the record - True
+        has_purchased = OrderItem.objects.filter(order__user=request.user, product=product).exists()
+
+        if not has_purchased:
+            messages.error(request, "You can write the review only after you buy product.")
+            return redirect('products:product_detail', slug=product.slug)
+        
+        #from POST-data dict, get value from key "rating" or "comment"
+        rating = request.POST.get('rating')
+        comment = request.POST.get('comment')
+        
+        Review.objects.create(product=product, user=request.user, rating=rating, comment=comment)
+
+        return redirect("products:product_detail", slug=product.slug)
