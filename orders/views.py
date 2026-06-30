c05a3eb1d325a3d946bd8482adf3e0480763823b
Daniel <danil.malaev3@gmail.com>
2026-06-30 19:23:09 +0300

    add: detail & add & remove & update handlers

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views import View
+from products.models import Product
+from .cart import Cart
 
-# Create your views here.
+class CartDetailView(View):
+
+    def get(self, request):
+        cart = Cart(request)
+        return render(request, 'orders/cart.html', {'cart': cart})
+
+
+class CartAddView(View):
+
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        quantity = int(request.POST.get('quantity', 1))
+        cart.add(product, quantity)
+        return redirect('orders:cart_detail')
+
+class CartRemoveView(View):
+
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        cart.remove(product)
+        return redirect('orders:cart_detail')
+
+class CartUpdateView(View):
+
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        quantity = int(request.POST.get('quantity', 1))
+        cart.update(product, quantity)
+        return redirect('orders:cart_detail')
