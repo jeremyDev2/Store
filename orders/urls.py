e80de897f5082cddec8648c4d60ed19618d8b85a
Daniel <danil.malaev3@gmail.com>
2026-07-01 22:06:05 +0300

    add: order create url

--- orders/urls.py
+++ orders/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
     path("cart/remove/<int:product_id>/", views.CartRemoveView.as_view(),name='cart_remove'),
     path('cart/update/<int:product_id>/', views.CartUpdateView.as_view(),name='cart_update'),
+    path('checkout/', views.OrderCreateView.as_view(), name="checkout"),
 ]
