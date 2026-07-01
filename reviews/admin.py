2009fb177935ed866cebc595fa74450ac2ff9ded
Daniel <danil.malaev3@gmail.com>
2026-07-01 22:08:55 +0300

    review admin filter

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from reviews.models import Review
 
-admin.site.register(Review)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = ['product', 'user', 'rating', 'created_at']
+    #rating filter
+    list_filter = ['rating']
+    #product & user search
+    search_fields = ['product__name', 'user__username']
