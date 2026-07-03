dc680259f441b899aac25516d894061ad32b9159
Daniel <danil.malaev3@gmail.com>
2026-07-03 23:36:37 +0300

    add: 'graphql/' schema path

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from graphene_django.views import GraphQLView
+from config.ql_schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,5 @@ urlpatterns = [
     path('orders/', include("orders.urls")),
     path('users/', include('users.urls')),
     path('api/', include('config.api_urls')),
+    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
 ]
