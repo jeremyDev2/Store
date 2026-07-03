3013741f4dd14be24dba7a71c632bfe3f35b187c
Daniel <danil.malaev3@gmail.com>
2026-07-03 23:33:54 +0300

    fix: path syntax - '/'

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'reviews'
 
 urlpatterns = [
-    path('add/<int:product_id/>', views.AddReviewView.as_view(), name='add_review')
+    path('add/<int:product_id>/', views.AddReviewView.as_view(), name='add_review')
 ]
