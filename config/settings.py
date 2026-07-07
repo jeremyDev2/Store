acd1cbc1b91df1bc56492fb7025e35d80780b530
Daniel <danil.malaev3@gmail.com>
2026-07-07 13:30:59 +0300

    fix: use STORAGES instead of DEFAULT_FILE_STORAGE for Django 4.2+

--- config/settings.py
+++ config/settings.py
@@ -153,4 +153,11 @@ CLOUDINARY_STORAGE = {
     'API_KEY': config('CLOUDINARY_API_KEY'),
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    'default': {
+        'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
